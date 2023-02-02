4d7dbbc696fa8ca287ef579531eebbbe9273b859
Rei-Suzuki1729 <eva2021end@gmail.com>
2023-02-02 14:06:36 +0900

    create station4

--- src/App.js
+++ src/App.js
@@ -7,12 +7,17 @@ import './App.css'
  * 
  * @type {React.FC}
  */
+
+
 export const App = () => {
+  const [dogUrl, setDogUrl] = React.useState(
+    'https://images.dog.ceo/breeds/springer-english/n02102040_3640.jpg',
+  )
   return (
     <div>
       <header>Dogアプリ</header>
       <p>犬の写真載せるアプリです</p>
-      <img src="https://images.dog.ceo/breeds/springer-english/n02102040_3640.jpg"></img>
+      <img src={dogUrl}></img>
     </div>
   )
 }
