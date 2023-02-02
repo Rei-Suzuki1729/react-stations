6cb7e5defe08c41129e58e78f604b5ff76e72081
Rei-Suzuki1729 <eva2021end@gmail.com>
2023-02-02 14:16:22 +0900

    create station5

--- src/App.js
+++ src/App.js
@@ -13,11 +13,21 @@ export const App = () => {
   const [dogUrl, setDogUrl] = React.useState(
     'https://images.dog.ceo/breeds/springer-english/n02102040_3640.jpg',
   )
+
+  const fetchDog = () => {
+    fetch('https://dog.ceo/api/breeds/image/random')
+      .then((res) => res.json())
+      .then((data) => { 
+        setDogUrl(data.message)
+      })
+  }
   return (
     <div>
       <header>Dogアプリ</header>
       <p>犬の写真載せるアプリです</p>
       <img src={dogUrl}></img>
+      <br/>
+      <button onClick={fetchDog}>送信</button>
     </div>
   )
 }
